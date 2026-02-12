332c554359b397eb1aac2c373fe99bafde818245
Soon-1234 <sosolkjeon21@gmail.com>
2026-02-12 07:02:08 +0300

    try

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php
 session_start();
 
-require __DIR__ . '/app/vendor/autoload.php';  
+require __DIR__ . '/app/vendor/autoload.php';
+
+Flight::set('flight.views.path', __DIR__ . '/app/views');
+
+Flight::register('db', 'PDO', [
+    'mysql:host=localhost;dbname=takalo;charset=utf8',
+    'root',   // user XAMPP par défaut
+    '',       // password XAMPP par défaut (vide)
+    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+]);
 
 require __DIR__ . '/app/controllers/ObjectController.php';
+
 require __DIR__ . '/app/routes.php';
 
 Flight::start();
