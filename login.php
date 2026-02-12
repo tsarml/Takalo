cd960a1af4104e05ba82ab63d7b456ff45e4b3a0
Tsara <4090mlt@gmail.com>
2026-02-12 09:17:55 +0300

    -page login

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+<form action="#" method="post">
+    <h1>Login:</h1>
+    <p>Email : <input type="text" name="email"></p>
+    <p>Mot de passe : <input type="text"></p>
+
+    <input type="button" value="submit">
+</form>
+</body>
+</html>
